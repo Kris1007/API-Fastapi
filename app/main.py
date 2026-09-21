f789dd30300f5eaf4e1b1618da364a7622fd1b43
Kris1007 <krishnag4372@gmail.com>
2026-09-21 14:29:16 +0530

    Change in API

--- app/main.py
+++ app/main.py
@@ -42,7 +42,7 @@ app.include_router(vote.router)
 
 @app.get("/")
 async def root():
-    return {"message": "Welcome to my API! Live at krishnagupta.space 💫"}
+    return {"message": "Welcome to this amazing API! Live at krishnagupta.space 💫"}
 
 
 
